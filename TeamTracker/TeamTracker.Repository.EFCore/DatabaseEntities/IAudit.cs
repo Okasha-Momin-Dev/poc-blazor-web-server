bf4bd01d9e5d9597c13169e3eaecbac49c5a935c
Okasha Momin <okashamomin.dev@gmail.com>
2023-08-11 02:39:44 +0530

    Generic Repository added

---
+++ TeamTracker/TeamTracker.Repository.EFCore/DatabaseEntities/IAudit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTracker.Repository.EFCore.DatabaseEntities
+{
+    public interface IAudit
+    {
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
